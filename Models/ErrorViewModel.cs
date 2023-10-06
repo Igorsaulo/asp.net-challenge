3ee6e140fa35484534f7a46adbefe822691af601
igorsaulo <igorsaulo01@gmail.com>
2023-10-06 18:57:17 -0300

    create application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.Challenge.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
